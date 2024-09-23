f665755c9ac1acf55bf812bb15da82cbcef8eca1
Roy Francis <roymathewfrancis@gmail.com>
2024-09-23 17:28:12 +0200

    Updated for dardel

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 ## Build apptainer sif containers
 
+##SBATCH -A naiss2023-22-1027
 #SBATCH -A naiss2024-22-345
-#SBATCH -p core
+#SBATCH -p shared
 #SBATCH -n 6
-#SBATCH -t 4:00:00
+#SBATCH -t 1:30:00
 #SBATCH -J bld-sif
 
 starttime=`date +%s`
+
+module load PDC
+module load apptainer
 
 # set cache and temp directories to current wd
 export APPTAINER_CACHEDIR=${PWD}/APPTAINER_CACHEDIR
